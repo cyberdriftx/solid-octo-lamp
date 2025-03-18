8bc8559040e9c909f7f0e08f5d51f258ca274f99
Petro <cyberdriftx@gmail.com>
2025-03-18 22:02:36 +0100

    Create password_generator.js

---
+++ password_generator.js
@@ -0,0 +1,10 @@
+function generatePassword(length = 12) {
+    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        password += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return password;
+}
+
+console.log("🔑 Generated Password: " + generatePassword(16));
